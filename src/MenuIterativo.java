81cdc19e38e6b192c5c954c2987d89d31d1d8d09
fmendozaguti <fmendozaguti@gmail.com>
2025-10-29 15:54:07 -0500

    Agregue sistema de administración de cuentas

--- src/MenuIterativo.java
+++ src/MenuIterativo.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class MenuIterativo {
     public static void main(String[] args) {
-
+        var consola = new Scanner(System.in);
+        var salir = false;
+        while(!salir){
+            System.out.println("""
+                    *** SISTEMA DE ADMINISTRACIÓN DE CUENTAS
+                    Menú:
+                    1. Crear cuenta.
+                    2. Eliminar cuenta.
+                    3. Salir.
+                    Escoje la opción:\s
+                    """);
+            var opcion = consola.nextInt();
+            switch(opcion){
+                case 1 ->
+                    System.out.println("Creando tu cuenta...\n");
+                case 2 ->
+                    System.out.println("Eliminando tu cuenta...\n");
+                case 3 -> {
+                    System.out.println("Saliando tu cuenta...\n");
+                    salir = true;
+                }
+                default ->
+                    System.out.println("Opción invalida...\n");
+            }
+        }
+        System.out.println("Fin del sistema de administración de cuentas. Adios!");
     }
 }
